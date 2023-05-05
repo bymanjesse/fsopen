a77b6e4c4a984678c0adc4901cc9ad100d2138a2
Jesse Byman <jesseb@metropolia.fi>
2023-05-05 21:27:02 +0300

    Osa4 blogilista backend tehty

---
+++ osa4/models/blog.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+module.exports = mongoose.model('Blog', blogSchema)
