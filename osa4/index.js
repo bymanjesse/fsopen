aa1dba47923d9cf18de528c9d5e046fc24ce8aeb
Jesse Byman <jesseb@metropolia.fi>
2023-05-06 14:11:12 +0300

    Blogilistan toiminnallisuutta laajennettu ja testit niille tehty

--- osa4/index.js
+++ osa4/index.js
@@ -23,6 +23,7 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true 
 app.use(cors())
 app.use(express.json())
 
+// Blogien tulostus
 app.get('/api/blogs', async (req, res) => {
   try {
     const blogs = await Blog.find({})
@@ -33,6 +34,7 @@ app.get('/api/blogs', async (req, res) => {
   }
 })
 
+// Blogien postaus
 app.post('/api/blogs', async (request, response) => {
   const body = request.body
 
@@ -53,6 +55,41 @@ app.post('/api/blogs', async (request, response) => {
   } catch (error) {
     // Handle any errors that may occur during the save operation
     response.status(500).json({ error: 'An error occurred while saving the blog' })
+  }
+})
+
+// Blogien päivitys
+app.put('/api/blogs/:id', async (request, response) => {
+  const body = request.body
+
+  if (!body.title || !body.url) {
+    return response.status(400).json({ error: 'title or url missing' })
+  }
+
+  const updatedBlog = {
+    title: body.title,
+    author: body.author || "",
+    url: body.url,
+    likes: body.likes || 0
+  }
+
+  try {
+    const result = await Blog.findByIdAndUpdate(request.params.id, updatedBlog, { new: true })
+    response.json(result)
+  } catch (error) {
+    console.error(error)
+    response.status(500).json({ error: 'An error occurred while updating the blog' })
+  }
+})
+
+// Blogien poisto
+app.delete('/api/blogs/:id', async (request, response) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (error) {
+    console.error(error)
+    response.status(500).json({ error: 'An error occurred while deleting the blog' })
   }
 })
 
--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -1,16 +1,16 @@
 const supertest = require('supertest')
-const app = require('../index') // Import your app
+const app = require('../index')
 const api = supertest(app)
 const Blog = require('../models/blog')
 
 let server
 
 beforeAll((done) => {
-  server = app.listen(0, done) // Start the server on a random available port
+  server = app.listen(0, done) // Start the server
 })
 
 afterAll((done) => {
-  server.close(done) // Close the server after tests
+  server.close(done) // Close
 })
 
 describe('GET /api/blogs', () => {
@@ -24,35 +24,41 @@ describe('GET /api/blogs', () => {
   test('correct number of blogs is returned', async () => {
     const response = await api.get('/api/blogs')
     const blogsInDb = await Blog.countDocuments()
-  
+
     expect(response.body).toHaveLength(blogsInDb)
   })
 
   test('blogs have an "id" field instead of "_id"', async () => {
     const response = await api.get('/api/blogs')
     const firstBlog = response.body[0]
-  
+
     expect(firstBlog.id).toBeDefined()
     expect(firstBlog._id).not.toBeDefined()
   })
+})
+
+describe('POST /api/blogs', () => {
   test('a blog can be added and the number of blogs increases', async () => {
     // Get the initial list of blogs
     const initialBlogs = await api.get('/api/blogs')
 
     // Define a new blog to be added
     const newBlog = {
-      title: 'New Test Blog',
-      author: 'Test Author',
-      url: 'https://testblog.com',
+      title: 'POST Test Blog',
+      author: 'POST Author',
+      url: 'https://testiblogi.com',
       likes: 5,
     }
 
-    // Add the new blog using a POST request
-    await api
+    // Add the new blog
+    const response = await api
       .post('/api/blogs')
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/)
+
+    // Get the ID of the new blog
+    const newBlogId = response.body.id
 
     // Get the updated list of blogs
     const updatedBlogs = await api.get('/api/blogs')
@@ -63,45 +69,131 @@ describe('GET /api/blogs', () => {
     // Check that the new blog is in the updated list of blogs
     const titles = updatedBlogs.body.map(blog => blog.title)
     expect(titles).toContain('New Test Blog')
+
+    // Delete the test blog
+    await api.delete(`/api/blogs/${newBlogId}`).expect(204)
   })
+
   test('if likes is not provided, set it to 0', async () => {
     const newBlog = {
-      title: 'Test Blog',
+      title: 'Likes to 0',
       author: 'Test Author',
-      url: 'https://testblog.com'
+      url: 'https://testi.com'
     }
-  
+
+    // Post the new blog and get its ID
     const response = await api.post('/api/blogs').send(newBlog).expect(201)
-  
+    const newBlogId = response.body.id
+
+    // Check that the likes property is set to 0
     expect(response.body.likes).toBe(0)
+
+    // Delete the test blog
+    await api.delete(`/api/blogs/${newBlogId}`).expect(204)
   })
+
   test('POST /api/blogs fails without title and/or url', async () => {
     const newBlogWithoutTitle = {
-      author: 'Ada Lovelace',
-      url: 'https://example.com/blogpost-without-title'
+      author: 'Jesse Byman',
+      url: 'https://testi.com/without-title'
     }
     const newBlogWithoutUrl = {
       title: 'Blog post without URL',
       author: 'Grace Hopper'
     }
     const newBlogWithoutTitleAndUrl = {
-      author: 'Alan Turing'
+      author: 'Jere Byman'
     }
-  
+
     await api
       .post('/api/blogs')
       .send(newBlogWithoutTitle)
       .expect(400)
-  
+
     await api
       .post('/api/blogs')
       .send(newBlogWithoutUrl)
       .expect(400)
-  
+
     await api
       .post('/api/blogs')
       .send(newBlogWithoutTitleAndUrl)
       .expect(400)
+    })
   })
   
+describe('PUT /api/blogs/:id', () => {
+  test('a blog can be updated', async () => {
+  // Define a new blog to be added
+  const newBlog = {
+  title: 'Test Blog for Update',
+  author: 'Test Author',
+  url: 'https://testblog.com',
+  likes: 5,
+  }
+  // Add the new blog using a POST request
+  const postResponse = await api
+  .post('/api/blogs')
+  .send(newBlog)
+  .expect(201)
+  .expect('Content-Type', /application\/json/)
+
+  // Get the ID of the new blog
+  const blogToUpdate = postResponse.body
+
+  // Update the blog
+  const updatedBlogData = {
+  title: 'UPDATE test blog',
+  author: 'UPDATE Test Author',
+  url: 'https://updatedtest.com',
+  likes: 9
+  }
+
+  // Send the PUT request to update the blog
+  const putResponse = await api
+  .put(`/api/blogs/${blogToUpdate.id}`)
+  .send(updatedBlogData)
+  .expect(200)
+  .expect('Content-Type', /application\/json/)
+
+  // Check that the updated blog has the correct data
+  expect(putResponse.body.title).toEqual(updatedBlogData.title)
+  expect(putResponse.body.author).toEqual(updatedBlogData.author)
+  expect(putResponse.body.url).toEqual(updatedBlogData.url)
+  expect(putResponse.body.likes).toEqual(updatedBlogData.likes)
+
+  // Delete the test blog
+  await api.delete(`/api/blogs/${blogToUpdate.id}`).expect(204)
 })
+})
+
+describe('DELETE /api/blogs/:id', () => {
+  test('a blog can be deleted', async () => {
+  // Define a new blog to be added
+  const newBlog = {
+  title: 'Test Blog for DELETE',
+  author: 'Test Author',
+  url: 'https://testblog.com',
+  likes: 5,
+  }
+  // Add the new blog using a POST request
+  const postResponse = await api
+  .post('/api/blogs')
+  .send(newBlog)
+  .expect(201)
+  .expect('Content-Type', /application\/json/)
+
+  // Get the ID of the new blog
+  const blogToDelete = postResponse.body
+
+  // Delete the test blog
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+  // Check that the deleted blog is not in the list of blogs
+  const updatedBlogs = await api.get('/api/blogs')
+  const titles = updatedBlogs.body.map(blog => blog.title)
+  expect(titles).not.toContain(blogToDelete.title)
+  })
+})
+
+
